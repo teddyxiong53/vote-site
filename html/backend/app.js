d193ccf15544df2d6342319fd5ba86b071329c4c
teddyxiong53 <1073167306@qq.com>
2019-03-02 17:51:44 +0800

    2019年3月2日17:51:41

--- html/backend/app.js
+++ html/backend/app.js
@@ -1,14 +1,11 @@
-var http = require("http")
-var fs = require("fs")
+const express = require("express")
+var app = express()
 
-var server = http.createServer(function(req, res) {
-    req.on("data", function(data) {
-        console.log("服务器收到的数据：" + decodeURIComponent(data))
-    })
-    req.on("end", function() {
-        console.log("客户端请求数据全部接受完毕")
-    })
+app.post("/", function(req, res) {
+    console.log("req.params: " , req.params)
     res.end()
-}).listen(3344, "192.168.56.101", function() {
-    console.log("listen on 3344");
+})
+
+var server = app.listen(3344, "192.168.56.101", function() {
+    console.log("listen on 3344")
 })
